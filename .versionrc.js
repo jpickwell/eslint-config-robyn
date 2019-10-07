6fb221538df53bd24adb6a0f5a242ff5fc2e92eb
Jordan Pickwell <jpickwell@neuone.com>
2019-10-07 13:04:43 -0500

    build: add .versionrc that hides test/build

---
+++ .versionrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  types: [
+    {
+      type: 'build',
+      section: 'Build System',
+      hidden: true
+    },
+    {
+      type: 'chore',
+      hidden: true
+    },
+    {
+      type: 'ci',
+      hidden: true
+    },
+    {
+      type: 'feat',
+      section: 'Features'
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes'
+    },
+    {
+      type: 'test',
+      section: 'Tests',
+      hidden: true
+    }
+  ]
+}
