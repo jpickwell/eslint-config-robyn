838cdaf6449fce71f4246e9a730ed36913ca2319
Jordan Pickwell <jpickwell@neuone.com>
2021-09-23 12:34:58 -0500

    chore: update release config

--- .versionrc.js
+++ .versionrc.js
@@ -1,1 +1,55 @@
-module.exports = {};
+'use strict';
+
+module.exports = {
+  commitUrlFormat: '{{host}}/{{owner}}/{{repository}}/-/commit/{{hash}}',
+  compareUrlFormat:
+    '{{host}}/{{owner}}/{{repository}}/-/compare/{{previousTag}}...{{currentTag}}',
+  issueUrlFormat: '{{host}}/{{owner}}/{{repository}}/-/issues/{{id}}',
+  userUrlFormat: '{{host}}/{{user}}',
+  types: [
+    {
+      type: 'feat',
+      section: 'Features',
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes',
+    },
+    {
+      type: 'docs',
+      section: 'Documentation',
+    },
+    {
+      type: 'style',
+      hidden: true,
+    },
+    {
+      type: 'refactor',
+      hidden: true,
+    },
+    {
+      type: 'perf',
+      section: 'Performance Improvements',
+    },
+    {
+      type: 'test',
+      hidden: true,
+    },
+    {
+      type: 'build',
+      hidden: true,
+    },
+    {
+      type: 'ci',
+      hidden: true,
+    },
+    {
+      type: 'chore',
+      hidden: true,
+    },
+    {
+      type: 'revert',
+      section: 'Reverts',
+    },
+  ],
+};
